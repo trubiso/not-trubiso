a5710b9030fd5b2b588536eebeda36e3db9070f1
trubiso <crishipohola@gmail.com>
2021-09-02 01:03:16 +0200

    agerg

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ categoryFiles.forEach(file => loadModule(file, handler));
 client.once('ready', () => {
     console.log(`Logged in as ${client.user?.tag}!`);
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    // if (client.guilds.cache.get("717683408012181505")) (client.channels.cache.get("717683408553377815")! as TextChannel).send(`i'm bakke!!! ${e.happy.e}`);
+    if (client.guilds.cache.get("717683408012181505")) (client.channels.cache.get("717683408553377815")! as TextChannel).send(`i'm bakke!!! ${e.happy.e}`);
     client.user?.setPresence({ activities: [{ name: "yu !!", type: "LISTENING" }]});
     findPollMessages(handler).then(v => {
         const restoredPolls = v.map(v => Poll.restorePollFromMessage(v));
@@ -42,9 +42,9 @@ client.on('messageCreate', async (msg: Message) => {
         }
     }
 
-    if (handler.connectGames.some(v => v.channel?.id === msg.channelId)) {
+    /*if (handler.connectGames.some(v => v.channel?.id === msg.channelId)) {
         handler.connectGames.find(v => v.channel?.id === msg.channelId)?.handleMessage(msg, handler);
-    } else if (!msg.author.bot && msg.content.startsWith(handler.prefix)) {
+    } else */if (!msg.author.bot && msg.content.startsWith(handler.prefix)) {
         await handleCommand(msg, handler);
     }
 });
