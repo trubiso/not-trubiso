5f8b12c790e0f974909daaec395b5a6256729a1c
trubiso <crishipohola@gmail.com>
2021-09-02 14:14:44 +0200

    fixed custom emote regex

--- src/utils/customEmoteRegex.ts
+++ src/utils/customEmoteRegex.ts
@@ -1,4 +1,4 @@
 /**
  * A regular expression pattern that matches all custom emotes in Discord.
  */
-export const customEmoteRegex = /<[a]*?:[0-9a-zA-Z]{2,32}:[0-9]{18}>/g;
+export const customEmoteRegex = /<[a]*?:[0-9a-zA-Z_]{2,32}:[0-9]{18}>/g;
