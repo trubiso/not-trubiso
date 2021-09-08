b2b52ced1ff049a91391e9cf4ef41ee832c8f4c0
trubiso <crishipohola@gmail.com>
2021-09-09 00:39:26 +0200

    webhuk

---
+++ src/categories/smilie.ts
@@ -0,0 +1,9 @@
+import { Module } from "../types/module";
+
+export = {
+    name: 'smilie',
+	help: {
+        name: 'smilie',
+		brief: 'comandes such as sendinge webhukes'
+	}
+} as Module;
---
+++ src/commands/smilie/sendwebhook.ts
@@ -0,0 +1,36 @@
+import { TextChannel } from "discord.js";
+import { Command } from "../../types/command";
+import { customEmoteRegex } from "../../utils/customEmoteRegex";
+
+// const { e } = require('../../vars.json');
+
+export = {
+	name: 'sendwebhook',
+    aliases: ['sw'],
+	help: {
+        category: 'smilie',
+		brief: 'send a webheuk !',
+		usage: 'sendwebhook <name> | <avatar URL / emote> | <text>'
+	},
+    async execute(message, args) {
+        const channel = message.channel as TextChannel;
+        const webhooks = await channel.fetchWebhooks();
+        let webhook = webhooks.find(v => v.name === "smilie");
+        if (!webhook) {
+            webhook = await channel.createWebhook("smilie");
+        }
+        const rawArgs = args.join(' ').split('|').map(v => v.trim());
+        const name = rawArgs[0];
+        const avatar = rawArgs[1].match(customEmoteRegex) ? "https://cdn.discordapp.com/emojis/" + rawArgs[1].split(':')[2].slice(0, -1) + "?v=1" : rawArgs[1];
+        const text = rawArgs[2];
+        if (!name || !avatar || !text) {
+            throw "giv me apropiet parematers !!";
+        }
+        await webhook.send({
+            username: name,
+            avatarURL: avatar,
+            content: text
+        });
+        await message.delete();
+    },
+} as Command;
