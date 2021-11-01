8642527d1efe8432f33f38404570fa527f151c53
trubiso <crishipohola@gmail.com>
2021-11-01 12:18:31 +0100

    beter fancify

--- src/commands/fun/fancify.ts
+++ src/commands/fun/fancify.ts
@@ -1,7 +1,7 @@
 import { Util } from "discord.js";
 import { Command } from "../../types/command";
 
-const { e, alphabets } = require('../../vars.json');
+const { e, alphabets, alphabetNames, fullAlphabetNames } = require('../../vars.json');
 
 function fancifyWord(word: string): string {
     const c: string[] = alphabets[Math.floor(Math.random()*(alphabets.length-1)+1)].split(',');
@@ -16,16 +16,29 @@ export = {
 	help: {
         category: 'fancify',
 		brief: 'fancifies yuor texte',
-		usage: 'fancify <text>',
+		usage: 'fancify [alphabet] <text> [-list]',
 	},
 	execute(message, args) {
         if (!args.length) {
             throw "giv me text to fancifie";
         }
+        if (args.includes("-list")) {
+            let out = "**List of alphabets:**";
+            (fullAlphabetNames as string[]).forEach((v, i) => {
+                out += `\n${v} (\`${alphabetNames[i]}\`) - ${alphabets[i].split(',').slice(0, 3).join('')}`;
+            });
+            return message.reply(Util.cleanContent(out, message.channel)); // clean da content just in case
+        }
+
         let text = Util.cleanContent(args.join(' ').trim(), message.channel);
-        text = text.split(' ').map(v => {
-            return fancifyWord(v);
-        }).join(' ');
+        if (alphabetNames.includes(args[0])) {
+            text = text.split(' ').slice(1).join(' ');
+            (alphabets[alphabetNames.indexOf(args[0])].split(',') as string[]).forEach((v, i) => text = text.replaceAll(alphabets[0].split(',')[i], v));
+        } else {
+            text = text.split(' ').map(v => {
+                return fancifyWord(v);
+            }).join(' ');
+        }
 
         if (text.length > 2000) throw `yur text is too bigege !! ${e.sad.e}`;
         else return message.reply(text);
