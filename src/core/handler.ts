a349825e059de4675dbc8458a6548826bf19128a
trubiso <trubiso@users.noreply.github.com>
2022-03-10 00:37:58 +0100

    makeshift button system

--- src/core/handler.ts
+++ src/core/handler.ts
@@ -1,4 +1,4 @@
-import { GuildMember, Message, TextChannel } from 'discord.js';
+import { ButtonInteraction, GuildMember, Interaction, Message, TextChannel } from 'discord.js';
 import Bot from '@core/bot';
 import { getBotReadyAnswer, pick } from '@core/utils';
 import { e } from '@core/vars';
@@ -69,6 +69,17 @@ export default class Handler {
       }
     } catch (error) {
       this.$error(msg, error);
+    }
+  }
+
+  public async $interaction(i: Interaction) {
+    // TODO: have an actually complete system. For now it will only work for games.
+    if (i.isButton()) {
+      let game;
+      if ((game = this.bot.games.find(v => v.channel?.id === i.channelId))) {
+        const commandData = Object.assign(i as ButtonInteraction, this.bot);
+        game.$button!(commandData);
+      }
     }
   }
 
