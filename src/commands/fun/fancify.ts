7b97cd2c3403b3924f921f7c01130d56e4c1dcf2
trubiso <trubiso@users.noreply.github.com>
2022-03-11 16:42:00 +0100

    improved fancify

--- src/commands/fun/fancify.ts
+++ src/commands/fun/fancify.ts
@@ -15,29 +15,42 @@ export = {
 
     const getSampleOfAlphabet = (i: number) => alphabets[i].split(',').slice(0, 3).join('');
 
-    if (args.includes('-list')) {
-      let out = '**List of alphabets:**\n';
-      out += fullAlphabetNames.map((v, i) => `${v} (\`${alphabetNames[i]}\`) - ${getSampleOfAlphabet(i)}`).join('\n');
-
-      return this.reply(Util.cleanContent(out, this.channel)); // clean da content just in case
-    }
-
     const regularAlphabet: string[] = alphabets[0].split(',');
 
-    function fancifyWord(word: string): string {
-      const chosenAlphabet: string[] = pick(alphabets.slice(1)).split(',');
+    function fancifyWord(word: string, alphabetIdx?: number): string {
+      let chosenAlphabet: string[] = [];
+      if (alphabetIdx !== undefined) chosenAlphabet = alphabets[alphabetIdx].split(',');
+      else chosenAlphabet = pick(alphabets.slice(1)).split(',');
       regularAlphabet.forEach((v, i) => (word = word.replaceAll(v, chosenAlphabet[i])));
 
       return word;
     }
 
-    let text = Util.cleanContent(args.join(' ').trim(), this.channel);
+    if (args[0] === '-list') {
+      let out = '**List of alphabets:**\n';
+      let nextText = '';
+      if (args.length > 1) nextText = args.slice(1).join(' ');
+      out += fullAlphabetNames
+        .map((v, i) =>
+          `${v} (\`${alphabetNames[i]}\`) - ${args.length > 1 ? fancifyWord(nextText, i) : getSampleOfAlphabet(i)}`)
+        .join('\n');
 
-    if (alphabetNames.includes(args[0])) {
+      return this.reply(Util.cleanContent(out, this.channel)); // clean da content just in case
+    }
+
+    let text = Util.cleanContent(args.join(' ').trim(), this.channel).normalize('NFD'); // normalize to NFD so á becomes a´ and it actually gets fancified.
+
+    if (args.length > 1 && (alphabetNames.includes(args[0]) || args[0] === 'random')) {
       text = text.split(' ').slice(1).join(' '); // remove the alphabet name
-      alphabets[alphabetNames.indexOf(args[0])]
-        .split(',')
-        .forEach((currentLetter, i) => (text = text.replaceAll(regularAlphabet[i], currentLetter)));
+      if (args[0] === 'random')
+        text = text
+          .split('')
+          .map(v => fancifyWord(v))
+          .join('');
+      else
+        alphabets[alphabetNames.indexOf(args[0])]
+          .split(',')
+          .forEach((currentLetter, i) => (text = text.replaceAll(regularAlphabet[i], currentLetter)));
     } else {
       text = applyPerWord(fancifyWord, text);
     }
