f291061b2a41702c9af1d9e021dd09e73aaadba1
trubiso <33161454+trubiso@users.noreply.github.com>
2023-05-02 17:53:00 +0200

    bring it back again

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Bot from '@core/bot';
 import { discordToken } from '@core/vars';
 
-const [, , ...args] = process.argv;
+const [,, ...args] = process.argv;
 const isDev = args.includes('--dev');
 
 const bot = new Bot('<', isDev);
